477fd6a12ed88da1484df28b51352dfc43063fc0
hhoow0093 <howarddai2005@gmail.com>
2024-09-12 22:19:50 +0700

    pertemuan 13

---
+++ pertemuan13/function.php
@@ -0,0 +1,62 @@
+<?php
+$koneksi = mysqli_connect("localhost", "root", "", "phpdasar");
+function query($perintah){
+    global $koneksi;
+    $data = [];
+    $result = mysqli_query($koneksi, $perintah);
+    while($row = mysqli_fetch_assoc($result)){
+        $data[] = $row;
+    }
+    return $data;
+} 
+
+function nambah($data){
+    global $koneksi;
+    // htmlspecialchars biar user tidak dapat memesukkan script html
+    $nama = htmlspecialchars($data["nama"]);
+    $nim = htmlspecialchars($data["nim"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+    $perintah = "INSERT INTO mahasiswa VALUES('', '$nama', '$nim', '$email', '$jurusan', '$gambar')";
+    mysqli_query($koneksi, $perintah);
+    return mysqli_affected_rows($koneksi);
+}
+
+function hapus($id){
+    global $koneksi;
+    $perintah = "DELETE FROM mahasiswa WHERE id = $id";
+    mysqli_query($koneksi, $perintah);
+    return mysqli_affected_rows($koneksi);
+}
+
+function update($data, $id){
+    global $koneksi;
+    // htmlspecialchars biar user tidak dapat memesukkan script html
+    $nama = htmlspecialchars($data["nama"]);
+    $nim = htmlspecialchars($data["nim"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+    $perintah = "UPDATE mahasiswa SET 
+                nama = '$nama', 
+                nim = '$nim',
+                email = '$email',
+                jurusan = '$jurusan',
+                gambar = '$gambar'
+                WHERE id = $id
+                ";
+    mysqli_query($koneksi, $perintah);
+    return mysqli_affected_rows($koneksi);
+}
+
+function search($kunci){
+    $perintah = "SELECT * FROM mahasiswa WHERE
+                nama LIKE '%$kunci%' OR
+                nim LIKE '%$kunci%' OR
+                email LIKE '%$kunci%' OR
+                jurusan LIKE '%$kunci%'
+                ";
+    return query($perintah);
+}
+?>
