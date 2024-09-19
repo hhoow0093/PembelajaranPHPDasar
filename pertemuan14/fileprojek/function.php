0d847d43e228101705efb13993440b15acc95219
hhoow0093 <howarddai2005@gmail.com>
2024-09-19 21:16:57 +0700

    pertemuan14

---
+++ pertemuan14/fileprojek/function.php
@@ -0,0 +1,136 @@
+<?php
+$koneksi = mysqli_connect("localhost", "root", "", "phpdasar");
+function query($perintah)
+{
+    global $koneksi;
+    $data = [];
+    $result = mysqli_query($koneksi, $perintah);
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+    return $data;
+}
+
+function nambah($data)
+{
+    global $koneksi;
+    // htmlspecialchars biar user tidak dapat memesukkan script html
+    $nama = htmlspecialchars($data["nama"]);
+    $nim = htmlspecialchars($data["nim"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+
+    //upload gambar
+    $gambar = upload();
+    if (!$gambar) {
+        return false;
+    }
+
+
+    $perintah = "INSERT INTO mahasiswa VALUES('', '$nama', '$nim', '$email', '$jurusan', '$gambar')";
+    mysqli_query($koneksi, $perintah);
+    return mysqli_affected_rows($koneksi);
+}
+
+function upload()
+{
+    // gak secure
+    //$namaFile = $_FILES["gambar"]["name"];
+    
+    
+    $ukuranFile = $_FILES["gambar"]["size"];
+    $error = $_FILES["gambar"]["error"];
+    $tempName = $_FILES["gambar"]["tmp_name"];
+
+    //cek jika tidak ada gambar diupload
+    if ($error === 4) {
+        echo    "<script>
+                alert('pilih gambar terlebih dahulu!');
+                </script>";
+        return false;
+    }
+
+    //ekstensi apakah file valid?
+    $ekstensiFileValid = ['jpg', 'jpeg', 'png'];
+    $ekstensiGambar = explode(".", $_FILES["gambar"]["name"]);
+    $ekstensiGambar = strtolower(end($ekstensiGambar));
+
+
+    if (!in_array($ekstensiGambar, $ekstensiFileValid)) {
+        echo    
+        "<script>
+        alert('anda salah memasukkan ekstensi file!');
+        </script>";
+        return false;
+    }
+    // ini lebih aman karena jika ada nama file yang sama dan berbeda foto, maka foto yang ditampilkan adalah foto yang sama
+    $_FILES["gambar"]["name"] = uniqid() . ".$ekstensiGambar"; 
+    $namaFile = $_FILES["gambar"]["name"];
+
+    // lebih besar dari 1 MB
+    if($ukuranFile > 1000000){
+        echo 
+        "
+        <script>
+        alert('ukuran file terlalu besar');
+        </script>
+        ";
+        return false;   
+    }
+
+    $imgDirectory = 'D:/buatSimpanXampp/htdocs/menyimpanfilephp/wpu-tutorial/pertemuan14/fileprojek/img/';
+
+    //lolos pengecekan, pengecekan, gambar siap di upload
+    move_uploaded_file($tempName, $imgDirectory . $namaFile);
+    return $namaFile;
+
+
+}
+
+function hapus($id)
+{
+    global $koneksi;
+    $perintah = "DELETE FROM mahasiswa WHERE id = $id";
+    mysqli_query($koneksi, $perintah);
+    return mysqli_affected_rows($koneksi);
+}
+
+function update($data, $id)
+{
+    global $koneksi;
+    // htmlspecialchars biar user tidak dapat memesukkan script html
+    $nama = htmlspecialchars($data["nama"]);
+    $nim = htmlspecialchars($data["nim"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambarLama = htmlspecialchars($data["gambarLama"]);
+
+    if($_FILES["gambar"]["error"] === 4){
+        $gambar = $gambarLama;
+
+    }else{
+        $gambar = upload();
+
+    }
+    $perintah = "UPDATE mahasiswa SET 
+                nama = '$nama', 
+                nim = '$nim',
+                email = '$email',
+                jurusan = '$jurusan',
+                gambar = '$gambar'
+                WHERE id = $id
+                ";
+    mysqli_query($koneksi, $perintah);
+    return mysqli_affected_rows($koneksi);
+}
+
+function search($kunci)
+{
+    $perintah = "SELECT * FROM mahasiswa WHERE
+                nama LIKE '%$kunci%' OR
+                nim LIKE '%$kunci%' OR
+                email LIKE '%$kunci%' OR
+                jurusan LIKE '%$kunci%'
+                ";
+    return query($perintah);
+}
---
+++ pertemuan14/fileprojek/nambah.php
@@ -0,0 +1,65 @@
+<?php
+require "function.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menambah data ke database</title>
+</head>
+
+<body>
+    <h1>Menambahkan ke database mahasiswa</h1>
+    <?php if (isset($_POST["submit"])) { ?>
+        <?php if (empty($_POST["nim"]) || empty($_POST["nama"]) || empty($_POST["email"]) || empty($_POST["jurusan"])) { ?>
+            <h2 style="color: red;"><b><i>Masukkan data yang lengkap!</i></b></h2>
+        <?php } else { ?>
+            <?php
+            if (nambah($_POST) > 0) {
+                echo
+                "<script>
+                    alert('mahasiswa telah ditambahkan!');
+                    document.location.href = 'index.php';
+                </script>";
+            } else {
+                echo
+                "<script>
+                    alert('mahasiswa gagal ditambahkan!');
+                    document.location.href = 'index.php';
+                </script>";
+            }
+            ?>
+
+        <?php } ?>
+    <?php } ?>
+    <form action="" method="post" enctype="multipart/form-data">
+        <ul>
+            <li>
+                <label for="nim">NIM:</label>
+                <input type="text" name="nim" id="nim">
+            </li>
+            <li>
+                <label for="nama">nama:</label>
+                <input type="text" name="nama" id="nama">
+            </li>
+            <li>
+                <label for="email">email:</label>
+                <input type="text" name="email" id="email">
+            </li>
+            <li>
+                <label for="jurusan">jurusan:</label>
+                <input type="text" name="jurusan" id="jurusan">
+            </li>
+            <li>
+                <label for="gambar">gambar:</label>
+                <input type="file" name="gambar" id="gambar">
+            </li>
+            <button type="submit" name="submit">tambahkan mahasiswa</button>
+        </ul>
+    </form>
+</body>
+
+</html>
